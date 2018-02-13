9eb46d8f187a78937d68e29e9be4f55d94b3252e
Chilja Speransky <magiclanzelot@gmx.net>
2018-02-13 16:18:56 +0100

    change card by class "hidden"/"visible"

--- js/game.js
+++ js/game.js
@@ -1,10 +1,25 @@
+
 const field = document.querySelector('.grid-container');
+let count = 0;
 
-field.addEventListener('click', changeColor);
-
-function changeColor(){
+field.addEventListener('click', function(event){
+    event.preventDefault();  
 	const clicked = event.target;
 	const item = clicked.firstElementChild;
+	
+	if(count < 2){
+		const clicked = event.target;
+	    const item = clicked.firstElementChild;
+		item.className = "visible";
+		count++;
+	} else {
+		for(i = 0; i < 2; i++){
+			visibleItem = document.querySelector('.visible');
+			visibleItem.setAttribute("class", "hidden");
+		}		
+		count = 0;
+	}
 
-	item.style.display= "grid";
-}
+	
+});
+
