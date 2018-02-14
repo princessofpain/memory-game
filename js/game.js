0f38343b6d5da0d0238f3bc012307b895a67bafe
Chilja Speransky <magiclanzelot@gmx.net>
2018-02-14 11:34:59 +0100

    set functionality of changing the cards

--- js/game.js
+++ js/game.js
@@ -1,25 +1,47 @@
 
 const field = document.querySelector('.grid-container');
 let count = 0;
+let clickEvent = 0;
+clickedCards = [];
+usedCards = [];
+item = [];
+field.addEventListener('click', changeCards);
 
-field.addEventListener('click', function(event){
-    event.preventDefault();  
+function changeCards(){
+	event.preventDefault();
 	const clicked = event.target;
-	const item = clicked.firstElementChild;
-	
-	if(count < 2){
-		const clicked = event.target;
-	    const item = clicked.firstElementChild;
-		item.className = "visible";
-		count++;
-	} else {
-		for(i = 0; i < 2; i++){
-			visibleItem = document.querySelector('.visible');
-			visibleItem.setAttribute("class", "hidden");
-		}		
-		count = 0;
-	}
+	item[clickEvent] = clicked.firstElementChild;
+	item[clickEvent].className = 'visible';
 
+	clickedCards[clickEvent] = clicked.className;
+	// usedCards[clickEvent] = clicked;
+	console.log(clickedCards[clickEvent] + clickEvent);
+	usedCards[clickEvent] = clicked;
 	
-});
+	count++;
+
+	if (count == '2'){
+		console.log(clickedCards[clickEvent] + ' ' + clickedCards[clickEvent-1]);
+		if (clickedCards[clickEvent] != clickedCards[clickEvent-1]){
+			const visibleFirstCard = item[clickEvent-1];
+			const visibleSecondCard = item[clickEvent];
+			visibleFirstCard.className = '.hidden';
+			visibleSecondCard.className = '.hidden';
+		} else {
+			const firstCard = usedCards[clickEvent-1];
+			firstCard.className = '.match';
+			const secondCard = usedCards[clickEvent]
+			secondCard.className = '.match';
+			console.log(firstCard.className + " " + secondCard.className);
+
+			// firstCard = usedCards[clickEvent];
+			// secondCard = usedCards[clickEvent-1];
+			// firstCard.removeEventListener('click', changeCards);
+			// secondCard.removeEventListener('click', changeCards);
+			// console.log("removed " + firstCard + clickEvent + secondCard + (clickEvent-1)); 
+		}	
+		count = 0;
+	}		
+	clickEvent++;		
+}
 
