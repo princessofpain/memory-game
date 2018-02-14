d8b8d46ce317c5035204a234c0be4b9be98dc6eb
Chilja Speransky <magiclanzelot@gmx.net>
2018-02-14 12:01:58 +0100

    clean code without comments etc.

--- js/game.js
+++ js/game.js
@@ -12,16 +12,11 @@ function changeCards(){
 	const clicked = event.target;
 	item[clickEvent] = clicked.firstElementChild;
 	item[clickEvent].className = 'visible';
-
 	clickedCards[clickEvent] = clicked.className;
-	// usedCards[clickEvent] = clicked;
-	console.log(clickedCards[clickEvent] + clickEvent);
 	usedCards[clickEvent] = clicked;
-	
 	count++;
 
 	if (count == '2'){
-		console.log(clickedCards[clickEvent] + ' ' + clickedCards[clickEvent-1]);
 		if (clickedCards[clickEvent] != clickedCards[clickEvent-1]){
 			const visibleFirstCard = item[clickEvent-1];
 			const visibleSecondCard = item[clickEvent];
@@ -31,14 +26,7 @@ function changeCards(){
 			const firstCard = usedCards[clickEvent-1];
 			firstCard.className = '.match';
 			const secondCard = usedCards[clickEvent]
-			secondCard.className = '.match';
-			console.log(firstCard.className + " " + secondCard.className);
-
-			// firstCard = usedCards[clickEvent];
-			// secondCard = usedCards[clickEvent-1];
-			// firstCard.removeEventListener('click', changeCards);
-			// secondCard.removeEventListener('click', changeCards);
-			// console.log("removed " + firstCard + clickEvent + secondCard + (clickEvent-1)); 
+			secondCard.className = '.match'; 
 		}	
 		count = 0;
 	}		
