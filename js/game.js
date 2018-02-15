7d2bdb87b2b64e7ecc5d525a59e4cf77dea9a8fd
Chilja Speransky <magiclanzelot@gmx.net>
2018-02-15 14:27:41 +0100

    animation click and match, fail is prepared but in comments

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 const field = document.querySelector('.grid-container');
 let count = 0;
 let clickEvent = 0;
-clickedCards = [];
+clickedCardsClasses = [];
 usedCards = [];
 item = [];
 field.addEventListener('click', changeCards);
@@ -10,41 +10,51 @@ field.addEventListener('click', changeCards);
 function changeCards(){
 	event.preventDefault();
 	// identification of the clicked card and the first child (item/ the other side of the card)
-	const clicked = event.target;
-	item[clickEvent] = clicked.firstElementChild;
-	// changing the other side of the card to be visible by changing the class in CSS
-	item[clickEvent].className = 'visible';
-	console.log(item[clickEvent].className);
+	let clicked = event.target;
 
-	// getting the class name of the recently clicked card and storing it in an array
-	clickedCards[clickEvent] = clicked.className;
-	// getting the clicked element and storing it in an array
-	usedCards[clickEvent] = clicked;
-	// incrementing the number of the guesses to allow max. 2
-	count++;
+	// skipping a click event for the container
+	if(clicked.className === 'grid-container'){
+		alert('Click on a card to play!');
+	} else {
+		// changing the other side of the card to be visible by changing the class in CSS	
+		item[clickEvent] = clicked.firstElementChild;
+		item[clickEvent].className = 'visible';
+		const visibleFirstCard = item[clickEvent-1];
+		const visibleSecondCard = item[clickEvent];
 
-	// special action when 2 guesses were done
-	if (count == '2'){
-		// if the class names are different the class of the two last recently moved cards is 
-		// changed back to .hidden
-		if (clickedCards[clickEvent] != clickedCards[clickEvent-1]){
-			const visibleFirstCard = item[clickEvent-1];
-			const visibleSecondCard = item[clickEvent];
-			visibleFirstCard.className = '.hidden';
-			visibleSecondCard.className = '.hidden';
-		} else {
-		// if there is a match the class of the two last recently moved cards is changed to .match 
-		// 	to avoid more click reactions or changes of this card
-			const firstCard = usedCards[clickEvent-1];
-			firstCard.className = '.match';
-			const secondCard = usedCards[clickEvent]
-			secondCard.className = '.match'; 
+		// getting the class name of the recently clicked card and the one before and storing 
+		// it in an array
+		clickedCardsClasses[clickEvent] = clicked.className;		
+		// getting the clicked element and storing it in an array
+		usedCards[clickEvent] = clicked;
+		const firstCard = usedCards[clickEvent-1];
+		const secondCard = usedCards[clickEvent];
+		// incrementing the number of the guesses to allow max. 2
+		count++;
+		// special action when 2 guesses were done
+		if (count == '2'){
+			// if the class names are different the class of the two last recently moved cards is 
+			// changed back to .hidden
+			if (clickedCardsClasses[clickEvent] != clickedCardsClasses[clickEvent-1]){
+				// firstCard.classList.toggle = 'fail';	
+				// secondCard.classList.toggle = 'fail';
+				firstCard.className = clickedCardsClasses[clickEvent-1];	
+				secondCard.className = clickedCardsClasses[clickEvent]; 
+				visibleFirstCard.className = 'hidden';
+				visibleSecondCard.className = 'hidden';	
+					
+			// if there is a match the class of the two last recently moved cards is changed to .match 
+			// 	to avoid more click reactions or changes of this cards	
+			} else {			
+				firstCard.className = 'match';	
+				secondCard.className = 'match'; 
+				console.log(firstCard.classList + " " + secondCard.classList);
+			}	
+		// count has to be set to 0 to allow 2 guesses max.
+			count = 0;
 		}	
-	// count has to be set to 0 to allow 2 guesses max.
-		count = 0;
+		//clickEvent has to be incremented to count through the arrays (arrays store the clicked cards 
+		//and the class names)	
+		clickEvent++;	
 	}	
-	//clickEvent has to be incremented to count through the arrays (arrays store the clicked cards 
-	//and the class names)	
-	clickEvent++;		
 }
-
