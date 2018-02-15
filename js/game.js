de3db26296455bc9e36beb67191681167a95b132
Chilja Speransky <magiclanzelot@gmx.net>
2018-02-15 15:15:59 +0100

    animation for a failed guess

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ field.addEventListener('click', changeCards);
 function changeCards(){
 	event.preventDefault();
 	// identification of the clicked card and the first child (item/ the other side of the card)
-	let clicked = event.target;
+	const clicked = event.target;
 
 	// skipping a click event for the container
 	if(clicked.className === 'grid-container'){
@@ -19,8 +19,6 @@ function changeCards(){
 		// changing the other side of the card to be visible by changing the class in CSS	
 		item[clickEvent] = clicked.firstElementChild;
 		item[clickEvent].className = 'visible';
-		const visibleFirstCard = item[clickEvent-1];
-		const visibleSecondCard = item[clickEvent];
 
 		// getting the class name of the recently clicked card and the one before and storing 
 		// it in an array
@@ -31,30 +29,31 @@ function changeCards(){
 		const secondCard = usedCards[clickEvent];
 		// incrementing the number of the guesses to allow max. 2
 		count++;
+		
 		// special action when 2 guesses were done
 		if (count == '2'){
 			// if the class names are different the class of the two last recently moved cards is 
 			// changed back to .hidden
 			if (clickedCardsClasses[clickEvent] != clickedCardsClasses[clickEvent-1]){
-				// firstCard.classList.toggle = 'fail';	
-				// secondCard.classList.toggle = 'fail';
-				firstCard.className = clickedCardsClasses[clickEvent-1];	
-				secondCard.className = clickedCardsClasses[clickEvent]; 
-				visibleFirstCard.className = 'hidden';
-				visibleSecondCard.className = 'hidden';	
-					
+				firstCard.className = 'fail';	
+				secondCard.className = 'fail';
+				setTimeout(function(){
+					console.log("I´m a break")
+					firstCard.firstElementChild.className = 'hidden';
+					secondCard.firstElementChild.className = 'hidden';	
+				}, 1000);				
+				// clearTimeout(timeout);
 			// if there is a match the class of the two last recently moved cards is changed to .match 
 			// 	to avoid more click reactions or changes of this cards	
 			} else {			
 				firstCard.className = 'match';	
 				secondCard.className = 'match'; 
-				console.log(firstCard.classList + " " + secondCard.classList);
 			}	
 		// count has to be set to 0 to allow 2 guesses max.
 			count = 0;
 		}	
 		//clickEvent has to be incremented to count through the arrays (arrays store the clicked cards 
-		//and the class names)	
-		clickEvent++;	
+		//and the class names)			
 	}	
+	clickEvent++;
 }
